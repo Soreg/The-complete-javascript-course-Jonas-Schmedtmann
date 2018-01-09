e9de2be47fd1638b7577865adcab427492565925
Soren Gravesen <S_Gravesen92@hotmail.com>
2018-01-09 23:07:42 +0100

    Added event handlers for dice rolling

--- DOM-Pig-Game/app.js
+++ DOM-Pig-Game/app.js
@@ -9,17 +9,27 @@ GAME RULES:
 
 */
 
-var scores, roundScores, activePlayer, dice;
+var scores, roundScores, activePlayerW;
 
-scores = [0,0];
+scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random()*6)+1;
+document.querySelector(".dice").style.display = "none";
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '1';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '1';
 
-var x = document.querySelector('#score-0');
-console.log(x);
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  // 1. Random number
+  var dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('.dice').style.display = 'none';
+  // 2. Display the result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = "block";
+  diceDOM.src = 'dice-' + dice + ".png";
+  
+  // 3. Update the round score IF the rolled number was NOT a 1
+});
