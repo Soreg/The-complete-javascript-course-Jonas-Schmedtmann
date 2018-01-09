a64daefafc954d473497b445e12bb4eb70b75782
Søren Gravesen <soreg@morningtrain.dk>
2018-01-09 13:07:14 +0100

    First DOM access and Manipulation

--- DOM-Pig-Game/app.js
+++ DOM-Pig-Game/app.js
@@ -8,3 +8,18 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScores, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random()*6)+1;
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+
+var x = document.querySelector('#score-0');
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
