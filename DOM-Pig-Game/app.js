5c4a6b846a6b1ae7e105e2b4caaa1b961b1fdb86
Soren Gravesen <S_Gravesen92@hotmail.com>
2018-01-12 00:30:24 +0100

    Added functionality to update score + changing current player

--- DOM-Pig-Game/app.js
+++ DOM-Pig-Game/app.js
@@ -17,19 +17,36 @@ activePlayer = 0;
 
 document.querySelector(".dice").style.display = "none";
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '1';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '1';
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
 document.querySelector(".btn-roll").addEventListener("click", function() {
   // 1. Random number
   var dice = Math.floor(Math.random() * 6) + 1;
 
   // 2. Display the result
-  var diceDOM = document.querySelector('.dice');
+  var diceDOM = document.querySelector(".dice");
   diceDOM.style.display = "block";
-  diceDOM.src = 'dice-' + dice + ".png";
-  
+  diceDOM.src = "dice-" + dice + ".png";
+
   // 3. Update the round score IF the rolled number was NOT a 1
+  if (dice !== 1) {
+    // add score
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    // next player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+  }
 });
